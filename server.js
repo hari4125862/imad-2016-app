0d69f38939e2056cf48a8c51efe748e07a820a80
hari4125862 <hari4125862@gmail.com>
2016-11-12 16:53:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/styleabout.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'styleabout.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
